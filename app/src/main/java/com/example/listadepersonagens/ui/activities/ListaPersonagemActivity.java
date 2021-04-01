ba7325f1604840ef9d5fbb1c8edefa9b2f28799f
luisRcarvalho <54163895+luisRcarvalho@users.noreply.github.com>
2021-04-01 17:27:27 -0300

    Aula 25/03 - onResume

--- app/src/main/java/com/example/listadepersonagens/ui/activities/ListaPersonagemActivity.java
+++ app/src/main/java/com/example/listadepersonagens/ui/activities/ListaPersonagemActivity.java
@@ -2,7 +2,9 @@ package com.example.listadepersonagens.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.listadepersonagens.R;
 import com.example.listadepersonagens.dao.PersonagemDAO;
+import com.example.listadepersonagens.model.Personagem;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -24,10 +27,6 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         setContentView( R.layout.activity_lista_de_personagem );
         setTitle( "Lista de Personagens" );
 
-        PersonagemDAO dao = new PersonagemDAO();
-
-
-
         FloatingActionButton botaoNovoPersonagem = findViewById( R.id.floatingActionButton );
         botaoNovoPersonagem.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -38,8 +37,6 @@ public class ListaPersonagemActivity extends AppCompatActivity {
 
         //List<String> personagem = new ArrayList<>( Arrays.asList("Alex", "Ken", "Ryu", "Chun Li"));
 
-        ListView listadePersonagens = findViewById(R.id.activity_main_lista_personagem );
-        listadePersonagens.setAdapter(new ArrayAdapter<>( this, android.R.layout.simple_list_item_1, dao.todos()) );
 
    /*     TextView primeiroPersonagem = findViewById( R.id.textView );
         TextView segundoPersonagem = findViewById( R.id.textView2 );
@@ -49,4 +46,25 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         segundoPersonagem.setText( personagem.get(1));
         terceiroPersonagem.setText( personagem.get(2));*/
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        PersonagemDAO dao = new PersonagemDAO();
+
+        ListView listadePersonagens = findViewById( R.id.activity_main_lista_personagem );
+        List<Personagem> personagens = dao.todos();
+        listadePersonagens.setAdapter( new ArrayAdapter<>( this, android.R.layout.simple_list_item_1, personagens ) );
+        listadePersonagens.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int posicao, long id) {
+                Personagem personagemEscolhido = personagens.get(posicao);
+                Log.i("posicao", "" + personagemEscolhido );
+                Intent vaiParaFormulario = new Intent(ListaPersonagemActivity.this, FormularioPersonagemActivity.class);
+                startActivity( vaiParaFormulario );
+            }
+        } );
+    }
 }
+
